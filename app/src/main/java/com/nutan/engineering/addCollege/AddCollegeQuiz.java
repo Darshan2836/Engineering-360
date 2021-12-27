4459e99b090b6e9a5bff113f0d3259c6ba16b36d
darsh <darshanudagire2001@gmail.com>
2021-12-27 12:12:16 +0530

    COMMIT 9

---
+++ app/src/main/java/com/nutan/engineering/addCollege/AddCollegeQuiz.java
@@ -0,0 +1,16 @@
+package com.nutan.engineering.addCollege;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nutan.engineering.R;
+
+public class AddCollegeQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_college_quiz);
+    }
+}
